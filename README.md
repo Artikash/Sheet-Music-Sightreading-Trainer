5b864fcf1441e371a28e9f15706066046e212e03
Akash Mozumdar <akashmozumdar@gmail.com>
2017-09-15 17:37:48 -0400

    added a bar that dictates when to play notes, also many bugfixes

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ First, allow microphone access. This will make a random sequence of 8 notes appe
 
 Website will automatically use your microphone to determine whether you are playing the correct note: once you do, the note will dissapear. You may then continue and play the next note.
 
-Once every note is played a new random sequence of 8 notes is generated. Rinse and repeat. Read and play the notes as fast as you can!
+If you check "limited time to play notes" and don't play the notes in time, they will turn red. You should continue to the next note once this happens.
 
-Thanks to Jonathan Bergknoff for the algorithm that converts microphone signals to notes!
+Once every note is played (or the bar reaches the end) a new random sequence of 8 notes is generated. Rinse and repeat. Read and play the notes as fast as you can!
+
+Thanks to Jonathan Bergknoff for the algorithm that converts microphone input to notes!
--- main.js
+++ main.js
@@ -5,8 +5,8 @@ var test_frequencies = [];
 var note_map =
 	["222E2L","214F2","206G2","198A2","190B2","182C3","174D3","166E3","158F3","150G3","143A3","135B3","127C4L"
 	,"111C4L","103D4","095E4","087F4","080G4","072A4","064B4","056C5","048D5","040E5","032F5","024G5","016A5L"];
-//The above encodes note info like so: first 3 digits represent y coord, letter represents note name
-//next digit represents octave, L is added at the end if a ledger line is needed.
+// The above encodes note info like so: first 3 digits represent y coord, letter represents note name
+// next digit represents octave, L is added at the end if a ledger line is needed.
 var current_note = "";
 var max_whitenoise = 0;
 var whitenoise_measurements = 0;
@@ -14,6 +14,9 @@ var notes_played = 0;
 var staff_notes = ["", "", "", "", "", "", "", ""];
 var min_note = 0;
 var max_note = 26;
+var bar = true; // bar in the code refers to the bar moving across the screen dictating when to play notes.
+var notes_passed = 0;
+var bar_duration = 30000;
 for (var i = 0; i < 72; i++) {
 	var note_frequency = C2 * Math.pow(2, i / 12);
 	var note_name = notes[i % 12] + octaves[Math.floor(i / 12)];
@@ -21,19 +24,24 @@ for (var i = 0; i < 72; i++) {
 	test_frequencies = test_frequencies.concat([note]);
 }
 
-document.getElementById("minnote").addEventListener("mouseup", update_note_range);
-document.getElementById("maxnote").addEventListener("mouseup", update_note_range);
 window.addEventListener("load", initialize);
 var correlation_worker = new Worker("correlation_worker.js");
 correlation_worker.addEventListener("message", interpret_correlation_result);
+document.getElementById("minnote").addEventListener("mouseup", update_note_range);
+document.getElementById("maxnote").addEventListener("mouseup", update_note_range);
+document.getElementById("barcheckbox").addEventListener("click", function toggle_bpm_field() {
+	if ($("#barcheckbox").prop("checked")) { $("[id^='bpm']").fadeIn(0); }
+	else { $("[id^='bpm']").fadeOut(0); }
+});
 
 function initialize() {
 	var get_user_media = navigator.getUserMedia;
 	get_user_media = get_user_media || navigator.webkitGetUserMedia;
 	get_user_media = get_user_media || navigator.mozGetUserMedia;
-	get_user_media.call(navigator, { "audio": true }, use_stream, function () { });
+	get_user_media.call(navigator, { "audio": true }, use_stream, function () { $("#loading").text("Error: Microphone Unavailable"); });
+	if (!$("#barcheckbox").prop("checked")) { $("[id^='bpm']").fadeOut(0); }
 	update_note_range();
-	$("[id^='sharp']").fadeOut(0);
+	use_bar();
 }
 
 function use_stream(stream) {
@@ -85,12 +93,12 @@ function interpret_correlation_result(event) {
 		maximum_magnitude = magnitudes[i];
 	}
 	if (whitenoise_measurements < 5) { // The white noisemeasurements make sure that white noise doesn't register as a note.
-		document.getElementById("loading").textContent = "Calibrating microphone:" + (whitenoise_measurements + 1) * 100 / 5 + "%"; 
+		$("#loading").text("Calibrating microphone:" + (whitenoise_measurements + 1) * 100 / 5 + "%"); 
 		whitenoise_measurements++;
 		if (max_whitenoise < maximum_magnitude) { max_whitenoise = maximum_magnitude; }
 	}
 	if (whitenoise_measurements === 5) { // Once enough data on whitenoise is gathered, generate sheet music and start listening.
-		document.getElementById("loading").textContent = "";
+		$("#loading").text("");
 		whitenoise_measurements++;
 		start_practice();
 	}
@@ -99,18 +107,24 @@ function interpret_correlation_result(event) {
 	var average = magnitudes.reduce(function (a, b) { return a + b; }, 0) / magnitudes.length;
 	var confidence = maximum_magnitude / average;
 	var confidence_threshold = 15; // empirical, arbitrary.
-	if (confidence > confidence_threshold && maximum_magnitude > max_whitenoise * 2) {
-		var dominant_frequency = test_frequencies[maximum_index];
-		var alt1 = test_frequencies[maximum_index + 12];
-		var alt2 = test_frequencies[maximum_index - 12]; //The algorithm can be off by 1 octave, so need these as workarounds.
-		console.log("expected" + current_note + "actual" + dominant_frequency.name);
-		if (dominant_frequency.name === current_note || alt1.name === current_note || alt2.name === current_note) { continue_practice(); }
+	if (confidence > confidence_threshold && maximum_magnitude > max_whitenoise * 3) {
+		var dominant_frequency = test_frequencies[maximum_index].name;
+		var alt1 = test_frequencies[maximum_index + 12].name; //The algorithm can be off by 1 octave, so need these as workarounds.
+		var alt2 = test_frequencies[maximum_index - 12]; //Sometimes this tries to access notes which don't exist so the name is only 
+		console.log("expected" + current_note + "actual" + dominant_frequency); // accessed at the end of the "or" so it doesn't bug out
+		if (dominant_frequency === current_note || alt1 === current_note || alt2.name === current_note) { continue_practice(true); }
 	}
 }
 
 function start_practice() {
+	bar = $("#barcheckbox").prop("checked");
+	bar_duration = 540000 / $("#bpm").val();
+	notes_passed = 0;
 	notes_played = 0;
+	$("#bar").css("left", "100px");
+	if (bar) { $("#bar").animate({ left: "550px" }, bar_duration, "linear", start_practice); }
 	$("[id^='note']").fadeIn(0);
+	$("[id^='sharp']").fadeOut(0);
 	$("[id^='note']").each(function (note_num) {
 		var note_info = note_map[Math.floor(+min_note + Math.random() * (max_note - min_note))]; // Decide which note to generate.
 		var temp_note = note_info.substring(3, 5); 
@@ -120,18 +134,30 @@ function start_practice() {
 		}
 		staff_notes[note_num] = temp_note;
 		this.style.top = parseInt(note_info.substring(0, 3), 10) + "px";
-		document.getElementById("sharp" + note_num).style.top = parseInt(note_info.substring(0, 3), 10) - 12 + "px";
+		$("#sharp" + note_num).css("top", parseInt(note_info.substring(0, 3)) - 12 + "px");
 		this.src = note_info.length === 6 ? "notewithline.png" : "note.png";
 	});
 	current_note = staff_notes[0];
 }
 
-function continue_practice() {
-	if (notes_played === 7) { start_practice(); }
+function continue_practice(success) {
+	if (notes_played === 7 && !bar) { start_practice(); }
 	else {
-		$("[id $=" + notes_played + "]").fadeOut(500);
+		if (success && (!bar || notes_played < notes_passed + 1)) { $("[id $=" + notes_played + "]").fadeOut(500); }
+		else if (success) { return; }
+		else { $("[id $=note" + notes_played + "]").prop("src", "rednote.png"); }
 		notes_played++; // Please note the order of these statements if you're going through the code in your head.
 		current_note = staff_notes[notes_played]; 
+	}
+}
+
+async function use_bar() {
+	while (true) {
+		await new Promise(resolve => setTimeout(resolve, 100));
+		if (parseInt($("#bar").css("left").substring(0, 3)) > 170 + notes_passed * 50) {
+			notes_passed++;
+			if (notes_passed > notes_played) { continue_practice(false); }
+		}
 	}
 }
 
@@ -140,7 +166,7 @@ async function update_note_range() {
 	min_note = $("#minnote").val();
 	max_note = $("#maxnote").val();
 	if (+min_note > +max_note) { $("#maxnote").val(+min_note + 1); update_note_range(); }
-	document.getElementById("minnotedisplay").textContent = "Lowest note: " + note_map[min_note].substring(3, 5);
-	document.getElementById("maxnotedisplay").textContent = "Highest note: E2";
-	document.getElementById("maxnotedisplay").textContent = "Highest note: " + note_map[max_note - 1].substring(3, 5);
+	$("#minnotedisplay").text("Lowest note: " + note_map[min_note].substring(3, 5));
+	$("#maxnotedisplay").text("Highest note: E2");
+	$("#maxnotedisplay").text("Highest note: " + note_map[max_note - 1].substring(3, 5));
 }
